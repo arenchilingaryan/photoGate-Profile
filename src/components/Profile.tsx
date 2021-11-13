22d7eb57940e010da06b130707f18d58a2db3cc5
arenchilingaryan <arenchilingaryan@yandex.ru>
2021-11-13 07:27:55 -0800

    add uncommited changes

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 const Profile = () => {
   return (
-    <div className="div" style={{fontSize: '100px'}}>
+    <div className="div" style={{ fontSize: '100px' }}>
       PROFILE
     </div>
   );
